3510f84f786d8002f2246895c93ce0c744d7c938
Alchimik981 <d.terementsev@yandex.ru>
2023-11-14 23:29:12 +0300

    made landing adaptive

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,10 +1,11 @@
-import "../Root/root.css";
 import Header from "../Header/header.jsx";
+import Landing from "../Landing/landing.jsx";
 
 function App() {
   return (
     <div className="root">
       <Header />
+      <Landing />
     </div>
   );
 }
