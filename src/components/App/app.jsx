1a32857caa84c3b1ad1a5f87142328beb42b1544
Alchimik981 <d.terementsev@yandex.ru>
2023-11-14 23:52:23 +0300

    made navigate adaptive

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,11 +1,13 @@
 import Header from "../Header/header.jsx";
 import Landing from "../Landing/landing.jsx";
+import Navigate from "../Navigate/navigate.jsx";
 
 function App() {
   return (
     <div className="root">
       <Header />
       <Landing />
+      <Navigate />
     </div>
   );
 }
---
+++ src/components/Navigate/navigate.jsx
@@ -0,0 +1,13 @@
+function Navigate() {
+  return (
+    <section className="navigate">
+      <ul className="navigate__links">
+        <li className="navigate__link">О проекте</li>
+        <li className="navigate__link">Технологии</li>
+        <li className="navigate__link">Студент</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Navigate;
