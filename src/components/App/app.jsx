11ec12b219fdceb16ca34ee70830d6d3b13c2428
Alchimik981 <d.terementsev@yandex.ru>
2023-11-19 21:02:34 +0300

    made 404 page adaptive

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -7,11 +7,18 @@ import SavedFilms from "../SavedFilms/SavedFilms.jsx";
 import Account from "../Account/Account.jsx";
 import Registration from "../Registration/Registration.jsx";
 import Login from "../Login/Login.jsx";
+import NotFoundPage from "../NotFoundPage/NotFoundPage.jsx";
+// import ProtectedRoute from "../ProtectedRoute/ProtectedRoute.jsx";
 
 function App() {
   return (
     <div className="root">
+      <NotFoundPage />
       <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+
       <Routes>
         <Route path="/profile" element={<Account />} />
       </Routes>
@@ -20,9 +27,6 @@ function App() {
       </Routes>
       <Routes>
         <Route path="/sign-up" element={<Registration />} />
-      </Routes>
-      <Routes>
-        <Route path="/" element={<Main />} />
       </Routes>
       <Routes>
         <Route path="/films" element={<SectionFilms />} />
